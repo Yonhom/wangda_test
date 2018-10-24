589bac63189a3e60998f0a27f40660554bfa7d4a
xuyonghong <yonhomx1006@gmail.com>
2018-10-24 14:11:40 +0800

    first commit

---
+++ WangdaTest/WangdaTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WangdaTest
+//
+//  Created by 徐永宏 on 2018/10/24.
+//  Copyright © 2018年 徐永宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
